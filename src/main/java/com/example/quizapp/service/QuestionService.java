0327e92f8d7d1d9aee2d592d5823b837cc7ae9f7
YaxPatel25 <np30201@gmail.com>
2025-09-28 10:46:03 +0530

    First Commit

---
+++ src/main/java/com/example/quizapp/service/QuestionService.java
@@ -0,0 +1,59 @@
+package com.example.quizapp.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.example.quizapp.dao.QuestionDao;
+import com.example.quizapp.model.Question;
+
+@Service
+public class QuestionService {
+
+	@Autowired
+	QuestionDao questionDao;
+	
+	public ResponseEntity<List<Question>> getAllQuestions() {
+		try{
+			return new ResponseEntity<>(questionDao.findAll(),HttpStatus.OK);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+	}
+
+	public ResponseEntity<List<Question>> getQuestionByCategory(String category) {
+		try{
+			return new ResponseEntity<>(questionDao.findByCategory(category),HttpStatus.OK);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+	}
+
+	public ResponseEntity<String> addQuestion(Question question) {
+		questionDao.save(question);
+		try {
+			return new ResponseEntity<>("Success",HttpStatus.CREATED);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return new ResponseEntity<>("Error", HttpStatus.BAD_REQUEST);
+	}
+
+	public ResponseEntity<String> deleteQuestion(Integer id) {
+		questionDao.deleteById(id);
+		try {
+			return new ResponseEntity<>("The question is delete with ID: " + id,HttpStatus.OK);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return new ResponseEntity<>("Error", HttpStatus.BAD_REQUEST);
+	}
+
+	
+}
